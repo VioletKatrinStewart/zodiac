6bdb704ff423e6f20798a0e157ee5644bec7f558
Violet Stewart <vkazoo2@gmail.com>
2021-11-30 17:57:08 -0800

    butts test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/butts/Footer";
 import Main from "./components/main/Main";
 
 function App() {
